6ef53d782945b600096b6bfafa623cdae09c9c0c
RominaMalimban <romina.lyn@hotmail.it>
2022-11-15 16:02:25 +0100

    funzione eseguita e invocata

--- Esercizio palindroma/js/script.js
+++ Esercizio palindroma/js/script.js
@@ -6,15 +6,30 @@
 let parola = prompt("Inserisci una parola");
 console.log("La parola inserita è", parola);
 
+let risultato = parPalindroma(parola);
+console.log(risultato);
+
+
 // creo la funzione per capire se è palindroma:
 function parPalindroma(word){
     // divido la parola in sottostringhe:
+    let parDivisa = word.split("");
+    console.log("La parola divisa è:", parDivisa);
 
     // inverto le lettere:
+    let parInvertita = parDivisa.reverse();
+    console.log("La parola invertita è", parInvertita);
 
     // riunisco le lettere:
+    let parFinale = parInvertita.join("");
+    console.log("La parola finale è:", parFinale);
 
     // verifico se è palindroma oppure no:
+    if(parFinale === parola){
+        return "è palindroma"
+    }else{
+        return "non è palindroma"
+    };
 }
 
 
