c65ad22111a4631a2771ee9bc28acd4acc4f8b3c
RominaMalimban <romina.lyn@hotmail.it>
2022-11-15 15:49:23 +0100

    variabile parola + commenti per la funzione

---
+++ Esercizio palindroma/js/script.js
@@ -0,0 +1,23 @@
+// Palidroma
+
+
+
+// chiedo all'utente di inserire una parola:
+let parola = prompt("Inserisci una parola");
+console.log("La parola inserita è", parola);
+
+// creo la funzione per capire se è palindroma:
+function parPalindroma(word){
+    // divido la parola in sottostringhe:
+
+    // inverto le lettere:
+
+    // riunisco le lettere:
+
+    // verifico se è palindroma oppure no:
+}
+
+
+
+
+// output
