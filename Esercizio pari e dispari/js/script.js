cb25701bc107a8aba1aa612dc649c5900368dfdf
RominaMalimban <romina.lyn@hotmail.it>
2022-11-15 14:55:27 +0100

    dichiarazione chi ha vinto

--- Esercizio pari e dispari/js/script.js
+++ Esercizio pari e dispari/js/script.js
@@ -9,6 +9,8 @@
 // scelgo il mio numero e lo salvo in una variabile:
 let mioNum = parseInt(prompt("Inserisci un numero da 1 a 5"));
 console.log("Il mio numero è", mioNum);
+let miaScelta = prompt("Scegli pari o dispari");
+console.log("La mia scelta è:", miaScelta);
 
 let numCpuRandom = creaNumCpu();
 console.log("Il numero del cpu è", numCpuRandom);
@@ -23,7 +25,7 @@ function creaNumCpu(){
 
 // sommo il mio numero e quello del pc:
 let somma = mioNum + numCpuRandom;
-console.log(somma);
+console.log("La somma dei due numeri è:", somma);
 
 let risultato = pariODispari();
 console.log("Il risultato è:", risultato);
@@ -31,13 +33,17 @@ console.log("Il risultato è:", risultato);
 // creo la funzione per stabilire se la somma è pari o dispari:
 function pariODispari(){
     if(somma % 2 === 0){
-        return "La somma dei due numeri è pari";
+        return "pari";
     }else{
-        return "La somma dei due numeri è dispari";
+        return "dispari";
     }
 }
 
 
 // dichiaro chi ha vinto:
-
+if( miaScelta === risultato ){
+    console.log("HAI VINTOOO");
+}else{
+    console.log("Mi dispiace ha vinto il pc")
+}
 
